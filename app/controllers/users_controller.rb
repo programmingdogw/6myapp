b61a6f92a18e411e32cbff932743bd71f45e18cf
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-02-05 13:33:10 +0900

    chapter7finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,5 +5,24 @@ class UsersController < ApplicationController
 
 
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welocome to the Sample App!"
+      # リダイレクト先のページでflashという変数に:successというkeyでアクセスした時のメッセージを代入
+      redirect_to @user
+      # redirect_to user_url(@user)と同じ意味。作成したユーザーのページに遷移する
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
