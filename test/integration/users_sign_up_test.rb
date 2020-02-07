cb11307c42483d31bb07a4e760d1bf883bc22fe2
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-02-08 02:23:41 +0900

    あ

--- test/integration/users_sign_up_test.rb
+++ test/integration/users_sign_up_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class UsersSignUpTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -28,6 +26,6 @@ class UsersSignUpTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.FILL_IN
+    assert_not flash[:danger]
   end
 end
