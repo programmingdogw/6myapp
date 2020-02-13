259ff4e1b1d7a9f0193dd247007d6dbf1b3a72d3
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-02-13 17:15:39 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
+  
 
   root 'static_pages#home'
   # rootを指定することで相対パスで指定が可能になるっぽい
-  
+  get 'sessions/new'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
