ff900c9fa616987492429bac18985a7be6a1e740
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-02-15 12:39:45 +0900

    a

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,4 +40,5 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+
 end
--- db/migrate/20200203121055_create_users.rb
+++ db/migrate/20200203121055_create_users.rb
@@ -4,6 +4,8 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :email
 
+      t.birthdate :birthdate not_null:, null:false
+      t.deathdate :deathdate not_null:, null:false
       t.timestamps
     end
   end
