eb5ecbe96884ae568f4280df99088ef9ee22ba32
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-02-16 13:26:50 +0900

    モデルとリレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200215075305) do
+ActiveRecord::Schema.define(version: 20200216040212) do
+
+  create_table "pictures", force: :cascade do |t|
+    t.string "bestimage"
+    t.string "image"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pictures_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
