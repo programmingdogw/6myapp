080dbd1082378e84062c6eaa6caff46fd66ed45f
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-02-19 16:13:14 +0900

    a
    aaa

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,11 @@ class UsersController < ApplicationController
 
   def show
     
-    @pictures = Picture.where(user_id: current_user.id)
-    @user = User.find_by(id:current_user.id)
+    
+    @user = User.find(params[:id])
 
+    @pictures = Picture.where(user_id: @user.id)
+    
   end
 
 
