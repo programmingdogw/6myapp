58b24df804ca1e098af1cbe8f77b574e5b351c72
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-02-21 15:51:57 +0900

    googlecharts導入

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,23 @@
 $(function(){
   console.log("hahahahaha")
-
-
-  
 });
+
+
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart);
+function drawChart() {
+    var data = google.visualization.arrayToDataTable([ //グラフデータの指定
+        ['Task', 'Hours per Day'],
+        ['Work',     11],
+        ['Eat',      2],
+        ['Commute',  2],
+        ['Watch TV', 2],
+        ['Sleep',    7]
+    ]);
+    var options = { //オプションの指定
+        pieSliceText: 'label',
+        title: 'Test Chart'
+    };
+    var chart = new google.visualization.PieChart(document.getElementById('piechart')); //グラフを表示させる要素の指定
+    chart.draw(data, options);
+}
