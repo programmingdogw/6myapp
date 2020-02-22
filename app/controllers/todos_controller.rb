b317bb70b97e3b5459e2943f5e5ec6131404ff65
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-02-22 20:09:05 +0900

    実験ページで画像と同じようなことした

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,12 +1,28 @@
 class TodosController < ApplicationController
 
+  def index
+    @todo = Todo.new
+    @todos = Todo.where(user_id: params[:user_id])
+    @user = User.find_by(id: params[:user_id])
+  end
+  
   def new
     @todo = Todo.new
   end
 
   def create
+
+    Todo.create(todo_params)
+
+    # まだ作ってない
+    # redirect_to user_todos_path   
+
   end
 
+  private
+    def todo_params
+      params.require(:todo).permit(:todo, :user_id)
+    end
 
 
 end
