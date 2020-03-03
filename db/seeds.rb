7e0cf08a59674f023dffac2fa95e114435129812
programmingdogw <59106983+programmingdogw@users.noreply.github.com>
2020-03-03 13:39:48 +0900

    ゲーム以外ほぼ完成

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,18 +18,99 @@ User.create!(name:  "Example User watawata",
   workingtime: '8.0',
   admin:true)
 
-99.times do |n|
-name  = Faker::Name.name
-email = "example-#{n+1}@railstutorial.org"
-password = "password"
-User.create!(name:  name,
-    email: email,
-    password:              password,
-    password_confirmation: password,
-    birthdate: '1985-08-17',
-    deathdate: '2050-11-19',
-    sleepingtime: '7.0',
-    commutingtime: '1.0',
-    workingtime: '8.0',
-    )
-end
+# 99.times do |n|
+# name  = Faker::Name.name
+# email = "example-#{n+1}@railstutorial.org"
+# password = "password"
+# User.create!(name:  name,
+#     email: email,
+#     password:              password,
+#     password_confirmation: password,
+#     birthdate: '1985-08-17',
+#     deathdate: '2050-11-19',
+#     sleepingtime: '7.0',
+#     commutingtime: '1.0',
+#     workingtime: '8.0',
+#     )
+#   end
+
+
+Game.create!(
+  name: "ファイナルドラゴン3",
+  cost: "100"
+)
+
+Game.create!(
+  name: "ファンタジークエスト7",
+  cost: "50"
+)
+
+Game.create!(
+  name: "ダンシングサーガ2",
+  cost: "5"
+)
+
+Game.create!(
+  name: "岩マンX",
+  cost: "777"
+)
+
+Game.create!(
+  name: "デモンズ魂",
+  cost: "1000"
+)
+
+Game.create!(
+  name: "ブラックソウル3",
+  cost: "3000"
+)
+
+Game.create!(
+  name: "ブラックソウル2",
+  cost: " 100"
+)
+
+Game.create!(
+  name: "ブラックソウル1",
+  cost: "13000"
+)
+
+Game.create!(
+  name: "一匹狼",
+  cost: "250"
+)
+
+Game.create!(
+  name: "仮面４ゴールデン",
+  cost: "444"
+)
+
+Game.create!(
+  name: "仮面５ロイヤル",
+  cost: "555"
+)
+
+Game.create!(
+  name: "仮面3フェス",
+  cost: "333"
+)
+
+Game.create!(
+  name: "仮面２罪",
+  cost: "222"
+)
+
+Game.create!(
+  name: "仮面２罰",
+  cost: "222"
+)
+
+Game.create!(
+  name: "ファイナルクエスト１",
+  cost: "10"
+)
+
+Game.create!(
+  name: "ドラゴンファンタジー",
+  cost: "30"
+)
